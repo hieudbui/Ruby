152d8cc782a3d118aa4f2c44f683a8b8fd14297d
Hieu Bui <hieu.bui@gmail.com>
2011-11-25 12:39:38 -0600

    asdfasf

--- metaprogramming.rb
+++ metaprogramming.rb
@@ -11,5 +11,35 @@ puts matz.speak.class
 
 
 matz2= Object.new
-matz2.speak
+#matz2.speak
 
+
+
+class A
+  def self.test
+    puts "class test"
+  end
+  def test
+    puts "instance test"
+  end
+end
+
+A.test
+A.new.test
+
+class B
+  class <<self
+    def test
+      puts "class test #{self}"
+    end
+  end
+  def test
+    puts "instance test #{self}"
+  end
+  def to_s
+    #puts "B instance"
+  end
+end
+
+B.test
+B.new.test
